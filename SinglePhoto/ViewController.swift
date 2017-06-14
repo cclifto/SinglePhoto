29990a4d2caea4abca424167b4bf73f00ffe033a
Conner Clifton <conner@ChaiOne-Standard-037.local>
2017-06-14 10:00:56 -0500

    Initial Commit

---
+++ SinglePhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by Conner Clifton on 6/14/17.
+//  Copyright © 2017 Conner Clifton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
